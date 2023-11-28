91937e0c9c0327e585082ec6fa70999762491ebd
Ayush Yadav <ayushydv100@gmail.com>
2023-11-29 03:06:03 +0530

    small fixes

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -8,13 +8,14 @@ export default function AboutMe() {
         <div className="hero--section--content">
           <h1 className="skills-section--heading">About Me</h1>
           <p className="hero--section-description">
-            A computer science professional well-versed in the MERN stack,
-            employing MongoDB for database management, ExpressJS and NodeJS for
-            server-side scripting, and ReactJS for creating interactive user
-            interfaces. Complemented by a solid foundation in Java and
-            proficiency in mobile app development using Flutter, my skills span
-            a diverse range of technologies, positioning me effectively for
-            challenging roles in the field of software development.
+            A computer science professional proficient in the MERN stack,
+            utilizing NodeJS and ExpressJS for server-side scripting, MongoDB
+            for database management, and ReactJS for creating interactive user
+            interfaces. In addition, I possess a strong foundation in Java and
+            MySQL, enhancing my capabilities in server-side development and
+            database management. Proficient in mobile app development using
+            Flutter, my diverse skill set positions me effectively for
+            challenging roles in the dynamic field of software development.
           </p>
         </div>
       </div>
